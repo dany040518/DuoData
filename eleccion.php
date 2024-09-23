083aa0308b9589cb0e69c93202e5ce36bd0a72a6
dany040518 <140460227+dany040518@users.noreply.github.com>
2024-09-23 11:34:50 -0500

    Delete eleccion.php

--- eleccion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $opcionSeleccionada = $_POST['opciones'];
-
-    if($opcionSeleccionada=='PYTHON'){
-        header("Location: python/python.html");
-    }else{
-        header("Location: java/java.html");
-    }
-?>
